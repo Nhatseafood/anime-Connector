940d237db85f6ccb307a5888bb3bf0bcfd8e61fd
Nhat Nguyen <nguyenhnhat@gmail.com>
2018-05-31 13:09:04 -0500

    Hidding keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://Nhat:Nhat@ds115768.mlab.com:15768/animeconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://Nhat:Nhat@ds115768.mlab.com:15768/animeconnector",
+  secretOrKey: "secret"
+};
